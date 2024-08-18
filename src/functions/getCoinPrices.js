06c96b2a440cc0199ec305f86e8cb157be2c6cc2
AkashJana18 <akashjana663@gmail.com>
2024-08-18 19:40:15 +0530

    T_T

--- src/functions/getCoinPrices.js
+++ src/functions/getCoinPrices.js
@@ -6,7 +6,7 @@ export const getCoinPrices = (id, days, priceType)=>{
         method: 'GET',
         url: `https://api.coingecko.com/api/v3/coins/${id}/market_chart`,
         params: {vs_currency: 'usd', days: `${days}`},
-        headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-n8EEf65W2g6Fp6E18YZbv2Mn'}
+        headers: {accept: 'application/json', 'x-cg-demo-api-key': process.env.COINGECKO_API_KEY}
     };
     
     const prices = axios
